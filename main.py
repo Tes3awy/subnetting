cdad7cbb2e4647b8b0165914bba9a56961f2fcde
Tes3awy <osaid2512@gmail.com>
2021-06-14 23:20:00 +0200

    Wait for subnet

--- main.py
+++ main.py
@@ -10,27 +10,29 @@ init(autoreset=True)
 
 
 def main():
-    # Take inputs from the user
+    input_subnets = ""
     try:
-        input_subnets = input(
-            "\n- Enter network subnet(s) in CIDR Notation (comma-seperated): "
-        )
+        # Take subnets from the user
+        while input_subnets == "":
+            input_subnets = input(
+                f"\n- Enter network subnet(s) in CIDR Notation (comma-seperated) [Required]: "
+            )
+        # Excel file name
         workbook_name = (
             input("- Name of Excel file w/o file extension? [Default IP-Schema.xlsx]: ")
             or "IP-Schema"
         )
+        # Excel sheet name
         worksheet_name = (
             input("- Worksheet name? [Default IP Schema Worksheet]: ")
             or "IP Schema Worksheet"
         )
+        # Do Subnetting
+        network_subnets = subnetting(input_subnets)
+        # Export subnetting results to an Excel file
+        export_subnets(network_subnets, workbook_name, worksheet_name)
     except KeyboardInterrupt:
         raise SystemExit(cprint("\nProcess interrupted by the user!", "yellow"))
-
-    # Do Subnetting
-    network_subnets = subnetting(input_subnets)
-
-    # Export subnetting results to an Excel file
-    export_subnets(network_subnets, workbook_name, worksheet_name)
 
     print("Done")
 
