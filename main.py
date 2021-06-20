523ffa4d55c5608b2b849591c50b7555d8a07697
Tes3awy <osaid2512@gmail.com>
2021-06-21 00:36:53 +0200

    Add gateway input

--- main.py
+++ main.py
@@ -19,7 +19,17 @@ def main():
         )
         if ".csv" not in input_subnets:
             raise SystemExit(
-                cprint("Sorry, but the input file MUST be a .csv file", "red")
+                cprint("Sorry! The input file MUST include .csv extension", "red")
+            )
+        gateway = int(
+            input("- Gateway first or last IP Address? [0/1] [Defaults to 0]: ") or "0"
+        )
+        if gateway not in (0, 1):
+            raise SystemExit(
+                cprint(
+                    "0 and 1 are the only allowed values! 0: First IP, 1: Last IP",
+                    "red",
+                )
             )
         # Excel file name
         workbook_name = (
@@ -27,21 +37,30 @@ def main():
             or "IP-Schema"
         )
         if ".xlsx" in workbook_name:
-            raise SystemExit(cprint("Please remove the .xlsx extension", "red"))
+            raise SystemExit(cprint("Oops! Please remove the .xlsx extension", "red"))
         # Excel sheet name
         worksheet_name = (
             input("- Worksheet name? [Defaults to IP Schema Worksheet]: ")
             or "IP Schema Worksheet"
         )
 
+        # Microsoft Excel does not allow worksheet name longer than 31 chars
+        if len(worksheet_name) > 31:
+            worksheet_name = worksheet_name[0:31]
+
         # Read CSV file
         subnets = read_subnets(input_subnets)
+
         # Do Subnetting
-        network_subnets = subnetting(subnets)
+        network_subnets = subnetting(subnets, gateway)
+
         # Export subnetting results to an Excel file
         export_subnets(network_subnets, workbook_name, worksheet_name)
-    except FileNotFoundError as e:
-        raise SystemExit(cprint(e, "red"))
+
+    except FileNotFoundError:
+        raise SystemExit(
+            cprint(f"main.py: {input_subnets} file does not exist!", "red")
+        )
     except KeyboardInterrupt:
         raise SystemExit(cprint("\nProcess interrupted by the user!", "yellow"))
 
